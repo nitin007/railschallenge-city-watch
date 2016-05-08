96d049eafcd8a7c5f3b61b65aafd6bb9bf9a192a
Nitin Gupta <nitinguptawebdev@gmail.com>
2016-05-08 06:43:24 +0530

    Finish responder show test

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,17 +1,30 @@
 class RespondersController < ApplicationController
-  before_filter :render_not_found, except: [:create, :index]
+  before_filter :render_not_found, only: [:new, :edit, :destroy]
   before_filter :build_emergency, only: [:create]
+  before_filter :fetch_responder, only: [:show, :update]
 
   def index
-    responders = Responder.all
-    render json: { responders: responders }
+    if asking_for_capacity?
+      render json: { capacity: Responder.capacities_per_group }
+    else
+      responders = Responder.all
+      render json: { responders: responders.as_json(only: [:emergency_code, :type, :name, :capacity, :on_duty]) }
+    end
   end
 
   def new
   end
 
-  def update
+  def show
+    if @responder
+      render json: { responder: @responder.as_json(only: [:emergency_code, :type, :name, :capacity, :on_duty]) }
+    else
+      render nothing: true, status: :not_found
+    end
+  end
 
+  def update
+    render json: { responder: @responder } if @responder.update(responder_params)
   end
 
   def create
@@ -48,5 +61,13 @@ class RespondersController < ApplicationController
 
     def build_emergency
       @responder = Responder.new(responder_params)
+    end
+
+    def fetch_responder
+      @responder = Responder.find_by(name: params[:id])
+    end
+
+    def asking_for_capacity?
+      params[:show].eql?('capacity')
     end
 end
--- app/models/responder.rb
+++ app/models/responder.rb
@@ -6,4 +6,11 @@ class Responder < ActiveRecord::Base
   def to_param
     name.parameterize
   end
+
+  def self.capacities_per_group
+    self.all.inject(Hash.new{|h, k| h[k] = [] }) do |hash, responder|
+      hash[responder.type] << responder.capacity
+      hash
+    end
+  end
 end
